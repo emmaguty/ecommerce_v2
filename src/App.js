d888c0301134e0b8b5e6a082785e7becda9b853d
emmanuelguty <manuel15901@gmail.com>
2023-03-09 10:43:05 -0600

    V2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
-import Sidebar from './components/Sidebar';
-import { Product } from './components/Product';
-import {Brand}  from './components/Brand';
-import {Category} from './components/Category';
-
-import ProductList from './components/ProductList';
-import BrandList from './components/BrandList';
-import CategoryList from './components/CategoryList';
-
-
-
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Sidebar />
-
-        <Switch>
-          <Route path='/products' exact>
-            <Product />
-          </Route>
-          <Route path='/products-list' exact>
-            <ProductList />
-          </Route>
-          <Route path='/brand' exact>
-            <Brand />
-          </Route>
-          <Route path='/brand-list' exact>
-            <BrandList />
-          </Route>
-          <Route path='/categorys' exacts>
-            <Category />
-          </Route>
-          <Route path='/categorys-list' exacts>
-            <CategoryList />
-          </Route>
-        </Switch>
-
-      </div >
-    </BrowserRouter>
+    <div>
+      <h1>Hello Friends</h1>
+    </div>
   );
 }
 
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,42 +1,9 @@
-import React, {useState } from 'react'
-import axios from 'axios';
+import React from 'react'
 
-export const Category = () => {
-
-  const [name, setName] = useState("");
-  const [image, setImage] = useState("");
-  // const [brand, setBrand] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    let formData = new FormData();
-    formData.append("name", name);
-    formData.append("image_url", image);
-    // formData.append("brand_url", brand);
-
-    const result = await axios.post('http://localhost:5050/category', formData)
-
-    alert(result.data);
-
-  }
-
+const Category = () => {
   return (
-    <div className='app__Product'>
-      <div className='app__Product-heading'>
-        <h1>Add Category</h1>
-      </div>
-      <form>
-        <div>
-          <label for="">Name</label>
-          <input onChange={(e) => setName(e.target.value)} type={"text"} name="" required />
-        </div>
-        <div>
-          <label for="">Image</label>
-          <input onChange={(e) => setImage(e.target.files[0])} type={"file"} name="" required />
-        </div>
-        <button onClick={handleSubmit}>Add Category</button>
-      </form>
-    </div>
+    <div>Category</div>
   )
 }
+
+export default Category
