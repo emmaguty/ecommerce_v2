32c786938defdb37ab8c8264aa3ee4ef3ebd14d1
emmanuelguty <manuel15901@gmail.com>
2023-03-10 00:03:21 -0600

    Category List Version 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import {Category} from './components/Category';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Hello Friends</h1>
+      <Category />
     </div>
   );
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
-const Category = () => {
+export const Category = () => {
+  const [categoryList, setCategoryList] = useState([])
+
+  const getCategoryList = async () => {
+    const result = await axios.get('http://localhost:5050/category')
+    setCategoryList(result.data)
+  }
+
+  useEffect(() => {
+    getCategoryList()
+  }, [])
+
   return (
-    <div>Category</div>
+    <div>
+      <h2>Shop Our Top Categories</h2>
+      <div className='container'>
+        {
+          categoryList.map(item => {
+            return (
+              <div className='category-item'>
+                <p>{item.category_name}</p>
+                <img src={`${item.category_image}`} alt={item.category_name} />
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
-}
 
-export default Category
+}
